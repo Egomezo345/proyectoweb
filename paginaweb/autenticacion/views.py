a7059b36c1decc175897d749b69a89345678fc21
Egomezo345 <9999999erick@gmail.com>
2023-12-20 20:41:43 -1200

    comentario personalizado

--- paginaweb/autenticacion/views.py
+++ paginaweb/autenticacion/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import View
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 # Create your views here.
 
 class VRegistro(View):
@@ -24,3 +24,11 @@ class VRegistro(View):
         else:
             return render(request, "registro/registro.html", {"form": form})
 
+def cerrar_sesion(request):
+    logout(request)
+
+    return redirect('home')
+
+def logear(request):
+    form=AuthenticationForm()
+    return render(request, "login/login.html", {"form": form})
