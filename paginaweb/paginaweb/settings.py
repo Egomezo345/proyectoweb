97f67f336e9b32905ccae247414bce4a6ea31951
Egomezo345 <9999999erick@gmail.com>
2023-12-20 13:38:49 -1200

    comentario personalizado

--- paginaweb/paginaweb/settings.py
+++ paginaweb/paginaweb/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'tienda',
     'contacto',
     'carro',
+    'autenticacion',
 ]
 
 MIDDLEWARE = [
--- paginaweb/paginaweb/urls.py
+++ paginaweb/paginaweb/urls.py
@@ -27,6 +27,8 @@ urlpatterns = [
 
     path('contacto/', include('contacto.urls')),
 
+    path('autenticacion/', include('autenticacion.urls')),
+
     path('tienda/', include('tienda.urls')),
 
     path('carro/', include('carro.urls')),
